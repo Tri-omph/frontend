c8537bf12ee239805f15c7a62fd51cf0e1e90369
cyrilclovis <cyrilclovis@gmail.com>
2025-02-17 20:36:28 +0100

    [REVERT] revert du commit 771bb838,  de la branche feat/ai-integration. En effet, l'IA est désormais dans le back

--- hooks/useAI.ts
+++ hooks/useAI.ts
@@ -49,28 +49,29 @@ const preprocessImageToTensor = async (uri: string) => {
 
     // Convertir l'image en tensor
     let imgTensor = decodeJpeg(raw);
-    imgTensor = tf.image.resizeBilinear(imgTensor, [224, 224]).toFloat();
+    imgTensor = tf.image.resizeNearestNeighbor(imgTensor, [224, 224]);
 
     // Normaliser l'image
     const mean = tf.tensor1d([0.485, 0.456, 0.406]);
     const std = tf.tensor1d([0.229, 0.224, 0.225]);
-    imgTensor = imgTensor.div(tf.scalar(255.0)).sub(mean).div(std);
+    imgTensor = imgTensor.div(255.0).sub(mean).div(std);
+
+    // Transposer l'image pour passer de NHWC à NCHW
+    imgTensor = imgTensor.transpose([2, 0, 1]);
 
     // Ajouter la dimension batch
     const imgWithBatch = imgTensor.expandDims(0);
+
+    // Libérer les tensors temporaires
+    mean.dispose();
+    std.dispose();
+    imgTensor.dispose();
+
     return imgWithBatch;
   } catch (error) {
     console.error("Error transforming image to tensor: ", error);
     return null;
   }
-};
-
-const makePredictions = async (
-  model: tf.GraphModel,
-  img: tf.Tensor<tf.Rank>,
-) => {
-  const predictions = await model.executeAsync(img);
-  return (predictions as tf.Tensor).dataSync();
 };
 
 const useAI = (
@@ -80,9 +81,8 @@ const useAI = (
   const [model, setModel] = useState<tf.GraphModel | null>(null);
   const [loading, setLoading] = useState(true);
   const [aiIsProcessing, setAiIsProcessing] = useState(false);
-  const [wasteTypePredictionResult, setWasteTypePredictionResult] = useState<
-    string | null
-  >(null);
+  const [wasteTypePredictionResult, setWasteTypePredictionResult] =
+    useState("");
 
   useEffect(() => {
     if (model) return; // Évite de recharger le modèle s'il est déjà disponible
@@ -96,6 +96,7 @@ const useAI = (
           bundleResourceIO(modelJson, modelWeights),
         );
         setModel(loadedModel);
+        setLoading(false);
       } catch (err) {
         console.log(
           "Une erreur est survenue lors du chargement du modèle d'IA. Veuillez réessayer plus tard. Détails : " +
@@ -107,8 +108,7 @@ const useAI = (
           "Une erreur est survenue lors du chargement du modèle d'IA. Veuillez réessayer plus tard. Détails : " +
             err,
         );
-      } finally {
-        setLoading(false); // S'assure que loading est mis à jour, même en cas d'erreur
+        setLoading(false);
       }
     };
 
@@ -130,11 +130,12 @@ const useAI = (
     image: CameraCapturedPicture,
     classes: string[] = RESULT_MAPPING,
   ) => {
+    if (!model) {
+      return null;
+    }
+
     try {
-      if (!model) return;
-
       const imgTensor = await preprocessImageToTensor(image.uri);
-
       if (!imgTensor || imgTensor.size === 0) {
         console.log(
           "L'image que vous avez fournie semble invalide. Veuillez essayer avec une autre photo.",
@@ -147,10 +148,9 @@ const useAI = (
         return null;
       }
 
-      const output = await makePredictions(model, imgTensor);
-      console.log(output);
+      const output = (await model.executeAsync(imgTensor)) as tf.Tensor;
 
-      if (output === undefined) {
+      if (output.size === 0) {
         console.log("l'IA n'a rien renvoyé. Veuillez réessayer.");
         showNotification(
           "error",
@@ -160,8 +160,10 @@ const useAI = (
         return null;
       }
 
-      const predictedClassIdx = output.indexOf(Math.max(...output));
+      const predictions = output.dataSync();
+      output.dispose();
 
+      const predictedClassIdx = predictions.indexOf(Math.max(...predictions));
       return classes[predictedClassIdx];
     } catch (error) {
       console.log(
@@ -180,8 +182,6 @@ const useAI = (
   const processWasteTypePrediction = async () => {
     if (aiIsProcessing) return; // Déjà en cours de traitement ...
 
-    console.log("Votre image est en cours d'analyse");
-
     setAiIsProcessing(true);
 
     if (model && capturedImage) {
@@ -191,20 +191,15 @@ const useAI = (
         "Le modèle a été chargé, l'analyse prendra quelques secondes...",
       );
       const prediction = await startPrediction(capturedImage);
-
       if (prediction) {
-        setWasteTypePredictionResult(prediction);
-        console.log(`C'est du ${prediction}`);
+        const highestPrediction = RESULT_MAPPING.indexOf(prediction);
+        setWasteTypePredictionResult(RESULT_MAPPING[highestPrediction]);
         showNotification(
           "success",
           "Résultat IA => SUPPP",
-          `C'est du ${prediction}`,
+          `C'est du ${RESULT_MAPPING[highestPrediction]}`,
         );
       }
-    } else {
-      console.log(
-        'Le model n\'est pas chargé. Relancer la requete en appuyant à noubeau sur "Analyser"',
-      );
     }
     setAiIsProcessing(false);
   };
@@ -212,7 +207,6 @@ const useAI = (
   const makeAIAvailable = () => {
     retakePicture();
     setAiIsProcessing(false);
-    setWasteTypePredictionResult(null);
   };
 
   return {
